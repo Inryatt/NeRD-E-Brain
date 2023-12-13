c6a375600f02c06d81cfc91523af590006cf7b80
Inryatt <56603542+Inryatt@users.noreply.github.com>
2023-12-13 09:56:43 +0000

    Delete consumer.py

--- consumer.py
+++
@@ -1,7 +0,0 @@
-from kafka import KafkaConsumer
-
-consumer = KafkaConsumer('nerdj_np',bootstrap_servers='localhost:9094',client_id="consumear_test", group_id='nerd-e')
-for msg in consumer:
-    print (msg)
-
-
